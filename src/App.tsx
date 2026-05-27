27b0e4eaabe141f1917e20e0d29b053ae975cd6c
haile-hash <lehonghaikt6@gmail.com>
2026-05-27 21:31:04 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -105,10 +105,10 @@ export default function App() {
   }
 
   const VAT_RATE = 0.1;
-  const EMAILJS_SERVICE_ID = "service_7ie990l";
-  const EMAILJS_TEMPLATE_ID = "template_m1j9i7k";
-  const EMAILJS_TEMPLATE_VIP_ID = "template_t91erhg";
-  const EMAILJS_PUBLIC_KEY = "5ric0kxuwNPlUleAv";
+  const EMAILJS_SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const EMAILJS_TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const EMAILJS_TEMPLATE_VIP_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_VIP_ID;
+  const EMAILJS_PUBLIC_KEY = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
   const IDLE_TIMEOUT = 5 * 60 * 1000; 
 
   // KHỞI TẠO EMAILJS
