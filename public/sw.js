82c0a0370c266713f91d6174f404ba4be06d58b6
haile-hash <lehonghaikt6@gmail.com>
2026-05-27 12:56:03 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,8 @@
+// public/sw.js
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Đã cài đặt thành công!');
+});
 
+self.addEventListener('fetch', (e) => {
+  // Bỏ trống để trình duyệt tự xử lý mạng bình thường
+});
